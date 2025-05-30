6da590d8d962f713c272c6ace7971bb954c5523c
FrancoBorba <franco.borba14@gmail.com>
2025-05-30 15:10:58 -0300

    First Commit

---
+++ src/main/java/com/francoborba/biblioteca/LibraryWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.francoborba.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWithSpringBootApplication.class, args);
+	}
+
+}
